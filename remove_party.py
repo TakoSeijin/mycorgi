f6afb99fd2549bbacf06c24c44dddd4e3682cc15
Seth Dandridge <sethdan@gmail.com>
2016-01-30 01:18:01 -0500

    fixing remove_party.py

--- remove_party.py
+++ remove_party.py
@@ -1,10 +1,10 @@
 from database import db_session
 from models import Orgy
 
-orgy_to_delete = 'radomds'
+party_to_delete = 'INSERT ORGY NAME HERE'
 
 party = db_session.query(Orgy)\
-                   .filter(Orgy.name.ilike(orgy_to_delete))\
+                   .filter(Orgy.name.ilike(party_to_delete))\
                    .filter(Orgy.is_old == False)\
                    .one_or_none()
 db_session.delete(party)
